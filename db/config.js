1c0c647f464537189b9caa60781f6dc15a476042
heretics7 <heretics7@naver.com>
2022-04-20 01:33:05 +0900

    node 셋팅, 폴더 위치 재설정

---
+++ db/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host : 'rds-demo-mysql.c92uyiveytek.us-east-1.rds.amazonaws.com',
+    user : 'admin',
+    password : 'Ksi10203040!',
+    port : '3306',
+    database : 'reactportfolio'
+}
